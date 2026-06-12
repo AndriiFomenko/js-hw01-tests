7e7eae59f1637bef5dd18cb423d5852507d71eb3
Alecto <fomenkoandrey1978@gmail.com>
2026-06-12 04:25:21 +0300

    feat: add test suite entry point for js-hw01-tests

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ console.log('#1. JavaScript homework example file')
 /*
  * Завдання #1
  *
- * Створіть змінні наступних типів даних зі вказаними значеннями:
- * - число (number)
- * - рядок (string)
- * - булеве значення (boolean)
- * - масив (array)
- * - об'єкт (object)
+ * Створіть змінні зі вказаними іменами та значеннями:
+ * - myNum: число (number) зі значенням 10
+ * - myStr: рядок (string) зі значенням 'some string'
+ * - myBool: булеве значення (boolean) зі значенням true
+ * - myArr: масив (array) з елементами [1, 2, 3, 4, 5]
+ * - myObj: об'єкт (object) з властивостями first: 'First Name' та last: 'Last Name'
  */
 var myNum = 10 // змінна myNum типу number зі значенням 10
 var myStr = 'some string' // змінна myStr типу string зі значенням 'some string'
@@ -31,6 +31,7 @@ console.log('myObj:', myObj)
  * Відформатуйте ціле число, яке зберігається в змінній myNum, 
  * щоб отримати результат з 2 знаками після коми.
  * Результат збережіть у змінній decimal2.
+ * (Зверніть увагу: результатом форматування буде рядок)
  */
 var decimal2 = myNum.toFixed(2) // змінна decimal2 містить число myNum з 2 знаками після коми
 console.log('decimal2:', decimal2)
@@ -39,7 +40,7 @@ console.log('decimal2:', decimal2)
  * Завдання #3
  *
  * Створіть змінну myBigInt і запишіть в неї число 123n (тип BigInt).
- * Потім збільшіть його на 1 та запишіть результат в цю ж змінну.
+ * Потім збільшіть його на 1n та запишіть результат в цю ж змінну.
  */
 var myBigInt = 123n // змінна myBigInt типу BigInt зі значенням 123n
 myBigInt = myBigInt + 1n // збільшення значення змінної myBigInt на 1n
